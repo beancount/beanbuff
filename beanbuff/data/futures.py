66c727592dfb7964820693272f75adca0c59e1f5
blais <blais@furius.ca>
2021-05-03 16:34:06 -0400

    futures: Added missing contract.

--- beanbuff/data/futures.py
+++ beanbuff/data/futures.py
@@ -8,57 +8,61 @@ from typing import Tuple
 OPTION_CONTRACT_SIZE = 100
 
 MULTIPLIERS = {
-    # Indices: S&P 500
-    '/ES'  : 50,
-    '/MES' : 5,
-    'SPX'  : 100,
+    # Indices : S&P 500
+    '/ES'     : 50,
+    '/MES'    : 5,
+    'SPX'     : 100,
 
-    # Indices: Nasdaq 100
-    '/NQ'  : 20,
-    '/MNQ' : 2,
-    'NDX'  : 100,
+    # Indices : Nasdaq 100
+    '/NQ'     : 20,
+    '/MNQ'    : 2,
+    'NDX'     : 100,
 
-    # Indices: Russell 2000
-    '/RTY' : 50,
-    '/M2K' : 5,
-    'RUT'  : 100,
+    # Indices : Russell 2000
+    '/RTY'    : 50,
+    '/M2K'    : 5,
+    'RUT'     : 100,
 
-    # Indices: Dow Jones
-    '/YM'  : 5,
-    '/MYM' : 0.5,
-    'DJI'  : 100,
+    # Indices : Dow Jones
+    '/YM'     : 5,
+    '/MYM'    : 0.5,
+    'DJI'     : 100,
 
     # FX
-    '/6E'  : 125_000,
-    '/6J'  : 12_500_000,
-    '/6A'  : 100_000,
-    '/6C'  : 100_000,
+    '/6E'     : 125_000,
+    '/6J'     : 12_500_000,
+    '/6A'     : 100_000,
+    '/6C'     : 100_000,
 
     # Energy
-    '/CL'  : 1000,
-    '/NG'  : 10_000,
+    '/CL'     : 1000,
+    '/NG'     : 10_000,
 
     # Metals
-    '/GC'  : 100,
-    '/SI'  : 5000,
-    '/HG'  : 25000,
+    '/GC'     : 100,
+    '/MGC'    : 10,
+    '/SI'     : 5000,
+    '/SIL'    : 1000,
+    '/HG'     : 25000,
+    #'/QC'    : 12500,
+    '/PA'     : 100,
 
     # Rates
-    '/ZQ'  : 4167,
-    '/GE'  : 2500,
-    '/ZT'  : 2000,
-    '/ZF'  : 1000,
-    '/ZN'  : 1000,
-    '/ZB'  : 1000,
+    '/ZQ'     : 4167,
+    '/GE'     : 2500,
+    '/ZT'     : 2000,
+    '/ZF'     : 1000,
+    '/ZN'     : 1000,
+    '/ZB'     : 1000,
 
     # Agricultural
-    '/ZC'  : 50,
-    '/ZS'  : 50,
-    '/ZW'  : 50,
+    '/ZC'     : 50,
+    '/ZS'     : 50,
+    '/ZW'     : 50,
 
     # Livestock
-    '/HE'  : 400,
-    '/LE'  : 400,
+    '/HE'     : 400,
+    '/LE'     : 400,
 }
 
 
@@ -84,6 +88,7 @@ MULTIPLIERS = {
 
 _TEMPORARY_MAPPING = {
     ('/SO', 'M'): ('/SI', 'N'),
+    ('/SO', 'N'): ('/SI', 'N'),
     ('/OG', 'N'): ('/GC', 'Q'),
     ('/EUU', 'M'): ('/6E', 'M'),
     ('/OZC', 'N'): ('/ZC', 'N'),
