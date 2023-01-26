e0dc572eb4d681cc88d8583a3aec1423a5f5e1ea
blais <blais@furius.ca>
2023-01-25 21:58:34 -0500

    Expanded Alerus handling

--- beanbuff/alerus/alerus_csv.py
+++ beanbuff/alerus/alerus_csv.py
@@ -2,19 +2,22 @@
 
 Go to
 
-  My Balance > History Browse; Go > Save History
+  Select Account > My Balance > History Browse > By Fund > Go > Save History
 
 then manually go to
 
-  My Balacne > Investment
+  My Balance > Investment
 
 and save balances for holdings.
+
+Note that selecting "By Activity" brings up a coarser aggregation.
 """
 __author__ = "Martin Blais <blais@furius.ca>"
 
 from os import path
-from typing import Optional
+from typing import Dict, Optional
 import datetime
+import re
 import sys
 
 import petl
@@ -28,12 +31,25 @@ from beancount.core import position
 from beangulp import petl_utils
 from beangulp import testing
 from beangulp import utils
+from beangulp import date_utils
 import beangulp
 
 
+CONFIG = {
+    "cash": "Account holding the cash margin",
+    "asset": "Account holding the main market asset",
+    "dividend": "Dividend income",
+    "interest": "Interest income",
+    "pnl": "PnL income",
+    "fee": "Management fees",
+}
+
+
 class Importer(beangulp.Importer):
-    def __init__(self, filing):
+    def __init__(self, filing: str, config: Dict[str, str]):
         self._account = filing
+        self.config = config
+        utils.validate_accounts(CONFIG, config)
 
     def account(self, filepath: str) -> data.Account:
         return self._account
@@ -46,7 +62,7 @@ class Importer(beangulp.Importer):
 
     def date(self, filepath: str) -> Optional[datetime.date]:
         _, max_date = (
-            petl.fromcsv(filepath).convert("Date", utils.parse_date).limits("Date")
+            petl.fromcsv(filepath).convert("Date", date_utils.parse_date).limits("Date")
         )
         return max_date + datetime.timedelta(days=1)
 
@@ -58,7 +74,19 @@ class Importer(beangulp.Importer):
 
         def create_transaction(r: petl.Record) -> data.Transaction:
             stock_account = account.join(self._account, r["Ticker"])
-            cash_account = account.join(self._account, "Cash")
+
+            # Select other account.
+            if re.match("^Contribution", r["Activity"]):
+                other_account = self.config['cash']
+            elif re.match("^Dividends", r["Activity"]):
+                other_account = self.config['dividend']
+            elif re.match("^Fee", r["Activity"]):
+                other_account = self.config['fee']
+            elif re.match("^Cash Earning", r["Activity"]):
+                other_account = self.config['interest']
+            else:
+                raise AssertionError(f'Invalid transaction type: {r["Activity"]}')
+
             meta = data.new_metadata(f"<{__file__}>".format, 0)
             meta["time"] = r["Time"]
             txn = data.Transaction(
@@ -81,7 +109,7 @@ class Importer(beangulp.Importer):
                         None,
                     ),
                     data.Posting(
-                        cash_account,
+                        other_account,
                         amount.Amount(-r["Value"], currency),
                         None,
                         None,
@@ -94,7 +122,7 @@ class Importer(beangulp.Importer):
 
         table = (
             petl.fromcsv(filepath)
-            .convert("Date", utils.parse_date)
+            .convert("Date", date_utils.parse_date)
             .convert("Sequence", int)
             .convert(["Price", "Shares", "Value"], utils.parse_amount)
             .sort(["Date", "Sequence"])
@@ -105,5 +133,12 @@ class Importer(beangulp.Importer):
 
 
 if __name__ == "__main__":
-    importer = Importer("Assets:US:Alerus")
+    importer = Importer(filing='Assets:US:Alerus', config={
+        'cash'     : 'Assets:US:Alerus:Cash',
+        'asset'    : 'Assets:US:Alerus:VTSAX',
+        'dividend' : 'Income:US:Alerus:VTSAX:Dividend',
+        'interest' : 'Income:US:Alerus:Interest',
+        'pnl'      : 'Income:US:Alerus:Pnl',
+        'fees'     : 'Expenses:Financial:Fees',
+    })
     testing.main(importer)
